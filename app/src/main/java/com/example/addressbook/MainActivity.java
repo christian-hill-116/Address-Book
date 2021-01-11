8ef8772563b4cb188100b20c7f7a84cc5397f184
CHRISTIAN HILL <christian.hill.116@gmail.com>
2021-01-10 17:39:02 -0700

    final

--- app/src/main/java/com/example/addressbook/MainActivity.java
+++ app/src/main/java/com/example/addressbook/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.addressbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
    private Button add, remove, edit, display, sort, search;
+    Button  btn_email, btn_dial, btn_call, btn_text, btn_maps;
+
+    EditText et_data;
+
+    final static int PERMISSION_TO_CALL= 1;
 
 
     @Override
@@ -62,6 +73,51 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //adding in Android Services..
+
+
+        et_data = findViewById(R.id.et_data);
+        btn_email = findViewById(R.id.btn_email);
+        btn_call = findViewById(R.id.btn_call);
+        btn_text = findViewById(R.id.btn_text);
+        btn_maps = findViewById(R.id.btn_maps);
+
+
+        btn_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String [] addresses = new String[1];
+                addresses[0] = et_data.getText().toString();
+                composeEmail(addresses, "Hello from Shad");
+            }
+        });
+
+
+        btn_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callPhoneNumber(et_data.getText().toString());
+            }
+        });
+
+        btn_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                composeMmsMessage(et_data.getText().toString(), "Free to talk?");
+            }
+        });
+
+        btn_maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mapsQuery = "geo:0,0?q=" + et_data.getText().toString();
+                Uri mapUri = Uri.parse(mapsQuery);
+                showMap(mapUri);
+            }
+        });
+
 
     }
 
@@ -95,7 +151,72 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //Android Services
 
+
+
+    public void composeEmail(String[] addresses, String subject) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+
+    public void callPhoneNumber(String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    PERMISSION_TO_CALL);
+        } else {
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_TO_CALL:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    callPhoneNumber(et_data.getText().toString());
+                }  else {
+                    Toast.makeText(this, "Cannot make a call without your permission", Toast.LENGTH_SHORT).show();
+                }
+                return;
+        }
+        // Other 'case' lines to check for other
+        // permissions this app might request.
+    }
+
+    public void composeMmsMessage(String phoneNumber, String message) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("smsto:" + phoneNumber));  // This ensures only SMS apps respond
+        intent.putExtra("sms_body", message);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 
 
 
